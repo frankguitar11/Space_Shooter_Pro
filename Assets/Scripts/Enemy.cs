ef3ac517f2f82d9fe26e7b278ba70bc79ad22e9f
frankguitar11 <frankgwarman@gmail.com>
2021-03-31 15:39:23 -0400

    Added Enemy Explosion VFX

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,11 +15,19 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] private int _enemyPointValue = 10;
 
+    private Animator _anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
+        if(_player == null)
+        {
+            Debug.LogError("Player is NULL");
+        }
+
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -50,7 +58,10 @@ public class Enemy : MonoBehaviour
                 player.DamagePlayer();
             }
 
-            Destroy(this.gameObject);
+            _anim.SetTrigger("OnEnemyDeath");
+            enemySpeed = 0.2f;
+
+            Destroy(this.gameObject, 2.7f);
         }
         else if(other.CompareTag("Laser"))
         {
@@ -62,7 +73,10 @@ public class Enemy : MonoBehaviour
                 _player.AddScore(_enemyPointValue);
             }
 
-            Destroy(this.gameObject);
+            _anim.SetTrigger("OnEnemyDeath");
+            enemySpeed = 0.2f;
+
+            Destroy(this.gameObject, 2.7f);
         }
     }
 }
